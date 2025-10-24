e6d784a31bcd51f890e2edc175d3f70781f2daa1
michaelkyalo <michaelkyalo876@gmail.com>
2025-10-24 09:55:07 +0300

    changes

--- back-end/config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "your_secret_key_here"
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "your_jwt_secret_key_here"
-    DEBUG = True
